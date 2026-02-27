3e3438186246249d33855e84453e736db0173043
Ding Liu <Baiding.Liu@noaa.gov>
2026-02-27 10:31:04 -0500

    try spack-stack 1.9.2 for HAFS-AR weather model build

--- modulefiles/ufs_gaea.intel.lua
+++ modulefiles/ufs_gaea.intel.lua
@@ -1,27 +1,27 @@
 help([[
   This module loads libraries required for building and running UFS Weather Model 
-  on the NOAA RDHPC machine Gaea C5 using Intel-2023.1.0.
+  on the NOAA RDHPC machine Gaea C6 using Intel-2024.2.1.
 ]])
 
 whatis([===[Loads libraries needed for building the UFS Weather Model on Gaea ]===])
 
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/upp-addon-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "2023.1.0"
-load(pathJoin("stack-intel", stack_intel_ver))
+stack_intel_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
 
-stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.25"
+stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.32"
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 
-stack_python_ver=os.getenv("stack_python_ver") or "3.10.13"
+stack_python_ver=os.getenv("stack_python_ver") or "3.11.7"
 load(pathJoin("stack-python", stack_python_ver))
 
-cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
 
 load("ufs_common")
 
-nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
+nccmp_ver=os.getenv("nccmp_ver") or "1.9.1.0"
 load(pathJoin("nccmp", nccmp_ver))
 
 unload("darshan-runtime")
