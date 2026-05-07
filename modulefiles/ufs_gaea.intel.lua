f11e494dff65da8d600d70f71bd11d12c176937b
Ding Liu <Baiding.Liu@noaa.gov>
2026-05-07 13:54:59 -0400

    upgrade to spack-stack-1.9.2 for consistency/support

--- modulefiles/ufs_gaea.intel.lua
+++ modulefiles/ufs_gaea.intel.lua
@@ -1,16 +1,16 @@
 help([[
   This module loads libraries required for building and running UFS Weather Model 
-  on the NOAA RDHPC machine Gaea C6 using Intel-2024.2.1.
+  on the NOAA RDHPC machine Gaea C6 using Intel-2023.2.0.
 ]])
 
 whatis([===[Loads libraries needed for building the UFS Weather Model on Gaea ]===])
 
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.8.0/envs/ue-intel-2021.10.0/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-intel-2023.2.0/install/modulefiles/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2023.2.0"
 load(pathJoin("stack-intel", stack_intel_ver))
 
-stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.29"
+stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.30"
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 
 stack_python_ver=os.getenv("stack_python_ver") or "3.11.7"
